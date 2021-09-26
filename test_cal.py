12118e1fb35e6b54895b9b8554fb5da84b2f6421
Aslan <asl.zurabov@gmail.com>
2021-09-26 17:14:37 +0300

    updated

--- test_cal.py
+++ test_cal.py
@@ -3,32 +3,32 @@ import test
 from test import Employee
 
 
-# class TestCalc(unittest.TestCase):
-#     def test_add(self):
-#         self.assertEqual(test.add(10, 5), 15)
-#         self.assertEqual(test.add(-1, 1), 0)
-#         self.assertEqual(test.add(-1, -1), -2)
-#
-#     def test_substract(self):
-#         self.assertEqual(test.substract(10, 5), 5)
-#         self.assertEqual(test.substract(-1, 1), -2)
-#         self.assertEqual(test.substract(-1, -1), 0)
-#
-#     def test_multiply(self):
-#         self.assertEqual(test.multiply(10, 5), 50)
-#         self.assertEqual(test.multiply(-1, 1), -1)
-#         self.assertEqual(test.multiply(-1, -1), 1)
-#
-#     def test_divide(self):
-#         self.assertEqual(test.divide(10, 5), 2)
-#         self.assertEqual(test.divide(-1, 1), -1)
-#         self.assertEqual(test.divide(-1, -1),1)
-#         self.assertEqual(test.divide(5, 2),2.5)
-#         with self.assertRaises(ValueError):
-#             test.divide(10,0)
-#
-# if __name__ == '__main__':
-#     unittest.main()
+class TestCalc(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(test.add(10, 5), 15)
+        self.assertEqual(test.add(-1, 1), 0)
+        self.assertEqual(test.add(-1, -1), -2)
+
+    def test_substract(self):
+        self.assertEqual(test.substract(10, 5), 5)
+        self.assertEqual(test.substract(-1, 1), -2)
+        self.assertEqual(test.substract(-1, -1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(test.multiply(10, 5), 50)
+        self.assertEqual(test.multiply(-1, 1), -1)
+        self.assertEqual(test.multiply(-1, -1), 1)
+
+    def test_divide(self):
+        self.assertEqual(test.divide(10, 5), 2)
+        self.assertEqual(test.divide(-1, 1), -1)
+        self.assertEqual(test.divide(-1, -1),1)
+        self.assertEqual(test.divide(5, 2),2.5)
+        with self.assertRaises(ValueError):
+            test.divide(10,0)
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 class TestEmployee(unittest.TestCase):
